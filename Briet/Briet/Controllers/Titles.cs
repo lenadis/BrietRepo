b1c9ba7ec8d5f61b671e2051c158980b9d0a5508
heiddish11 <heiddish11@ru.is>
2014-05-12 10:21:45 +0000

    Bjó til Titles controller

---
+++ Briet/Briet/Controllers/Titles.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Briet.Controllers
+{
+    public class Titles
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+    }
+}
